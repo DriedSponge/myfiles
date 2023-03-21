43e67e1e5c38a15c7ef99e46b84ac3c3037b3184
DriedSponge <jordan@driedsponge.net>
2023-03-20 23:39:57 -0700

    Installed Prettier

--- src/routes/[fileId]/+page.ts
+++ src/routes/[fileId]/+page.ts
@@ -1,20 +1,22 @@
-import type { PageLoad } from './$types';
+import type { PageLoad } from "./$types";
 import { Storage } from "appwrite";
 import AppwriteManager from "$lib/AppwriteManager";
 
 export const load = (async ({ url, params }) => {
-    const storage = new Storage(AppwriteManager.client);
-    try{
-        const file = await storage.getFile("files",params.fileId);
-        return {
-            found: true,
-            url: storage.getFileView("files",params.fileId),name:file.name, pageUrl:url,
-            type:file.mimeType,
-            download: storage.getFileDownload("files",file.name).href,
-            size: file.sizeOriginal,
-            created_at: file.$createdAt
-        }
-    }catch (e){
-        return {found: false}
-    }
+	const storage = new Storage(AppwriteManager.client);
+	try {
+		const file = await storage.getFile("files", params.fileId);
+		return {
+			found: true,
+			url: storage.getFileView("files", params.fileId),
+			name: file.name,
+			pageUrl: url,
+			type: file.mimeType,
+			download: storage.getFileDownload("files", file.name).href,
+			size: file.sizeOriginal,
+			created_at: file.$createdAt
+		};
+	} catch (e) {
+		return { found: false };
+	}
 }) satisfies PageLoad;
